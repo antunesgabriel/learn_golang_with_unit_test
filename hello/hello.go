f44c954edc0091e8f7a770110ed58cfc6d6e4f77
Gabriel Antunes <gabriel.antunes@compasso.com.br>
2021-10-04 09:07:09 -0300

    feature, hello word in any language using go with tdd

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,37 @@ package main
 
 import "fmt"
 
-const hello = "Hello"
+const helloDefault = "Hello"
+const helloSpanish = "Hola"
+const helloFrench = "Bonjour"
+const helloItalian = "Ciao"
 
-func Hello(name string) string {
-	return hello + ", " + name
+const spanish = "spanish"
+const french = "french"
+const italian = "italian"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "Word"
+	}
+
+	return switchPrefix(language) +  ", " + name
+}
+
+func switchPrefix (language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = helloSpanish
+	case french:
+		prefix = helloFrench
+	case italian:
+		prefix = helloItalian
+	default:
+		prefix = helloDefault
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Word"))
+	fmt.Println(Hello("Word", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,11 +3,57 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	name := "Gabriel"
-	expected := "Hello, " + name
-	result := Hello(name)
+	checkResult := func (t *testing.T, expected string, result string) {
+		t.Helper()
 
-	if result != expected {
-		t.Errorf("Expected: %s - But received: %s", expected, result)
+		if result != expected {
+			t.Errorf("Expected: %s - But received: %s", expected, result)
+		}
 	}
+
+	t.Run("say hello to people", func (t *testing.T) {
+		name := "Gabriel"
+		expected := "Hello, " + name
+		result := Hello(name, "")
+
+		checkResult(t, expected, result)
+	})
+
+	t.Run("Say hello word when the name is empty", func (t *testing.T) {
+		name := ""
+		expected := "Hello, Word"
+		result := Hello(name, "")
+
+		checkResult(t, expected, result)
+	})
+
+	t.Run("Say hello in spanish when you receive spanish language", func (t *testing.T) {
+		language := "spanish"
+		name := "Pablo"
+		expected := "Hola, " + name
+
+		result := Hello(name, language)
+
+		checkResult(t, expected, result)
+	})
+
+	t.Run("Say hello in french when you get french", func (t *testing.T) {
+		language := "french"
+		name := "Grizlley"
+		expected := "Bonjour, " + name
+
+		result := Hello(name, language)
+
+		checkResult(t, expected, result)
+	})
+
+	t.Run("Say hello in italian when you get italian", func (t *testing.T) {
+		language := "italian"
+		name := "Ramon"
+		expected := "Ciao, " + name
+
+		result := Hello(name, language)
+
+		checkResult(t, expected, result)
+	})
 }
